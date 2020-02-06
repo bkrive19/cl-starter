29d0d1b367edd84602f1d68e9f836f9e4e7b2b8b
Brandon <60404105+bkrive19@users.noreply.github.com>
2020-02-06 14:33:09 -0500

    Update Tera.py

--- Tera.py
+++ Tera.py
@@ -15,34 +15,77 @@ print('But then one day...')
 print("A rip was revealed in your world. This rip is something that'll change your race's way of living FOREVER.")
 print("You see Teragates gathering around this dimensional rip...")
 
+def firstbite():
+    print(" ")
+    print("Finally after years of boredom- your free! Your in a black place..")
+    print ("You see light. You walk towards it, but for some reason you feel lighter in mass.")
+    print("Your out! There's a lot of organic material- like in your world, but green and delicate, more alive. You realize that your're a ghost..")
+    print(" ")
+    answer = input ("You see a slow animal and a berry bush. Will you perfer a plant, or an easy meat meal?")
+    if answer == "meat"
+    print ("You ate the animal alive. You start to gain mass- your----------------")
+
+
+
+def rip():
+    print(" ")
+    print("Every one is just celebrating! You see some Teragates fall in the rip, and some jump in and out, but they seem tranparent now...")
+    answer = input ("You really want to see what's beyond the rip. go, or stay?")
+    if answer == "stay":
+        end1()
+    elif answer == "go": 
+        firstbite()
+        
+    else:
+        print("go or stay?")
+        rip()
+
+def birthplace():
+    print(" ")
+    print("You went back to the tree. The tree that gives birth to your race. You're the fruit of the tree. The only one now...")
+    print("You waited there for an hour. Waiting for something to happen. Then....")
+    print(" ")
+    print("The tree speaks! Saying in a soft accent,- YOU ARE THE ONLY CHILD I HAVE WHO CARED TO COME AND VISIT ME BEFORE LEAVING.")
+    print("I'M GLAD YOU KNOW WHO GAVE BIRTH TO YOU. SEAL THE RIP AND COME AND EAT WITH ME...")
+    from pyfiglet import Figlet
+
+    fig = Figlet(font='graffiti')
+    banner = fig.renderText("SECRET end")
+
+    print(banner)
+
+
+
+def end1():
+    print(" ")
+    print("You went back to go eat. After a couple of hours you noticed that everyone was gone... You realize your alone.")
+    wait()
+
+def wait():
+    answer = input ("Will you wait for them, or do you want to check out your mother tree? Check, or wait?")
+    if answer == "check":
+        print("You went to go check the tree that gae birth to you.")
+        birthplace()
+        
+    elif answer == "wait":
+        print("You waited a day later. No one came.")
+        wait()
+
+    else:
+        print ("check or wait?")
+        wait()
+        
 def gotorip():
     answer=input("Will you follow them? yes or no?")
     if answer == "yes":
          print("You Went to see what's up.")
          rip()
-         pass
+        
     elif answer =="no":
         print("You ignored the excitement.")
         end1()
     else:
         print("yes or no?")
-        gotorip()
-        pass
+        gotorip() 
 gotorip()
-pass
 
-def end1():
-    print("You went back to go eat. After a couple of hours you noticed that everyone was gone... You realize your alone.")
-    wait()
-    def wait():
-        answer = input ("Will you wait for them, or do you want to check out your mother tree? Check, or wait?")
-    if answer == "check":
-        print("You went to go check the tree that gae birth to you.")
-        birthplace()
-        pass
-    elif answer == "wait":
-        print("You waited a day later. No one came.")
-        wait()
-    else:
-        print ("check or wait?")
-        wait()
